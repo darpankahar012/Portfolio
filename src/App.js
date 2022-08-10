12a2e8ec295dea2a1373382f7e4711af1769f34b
darpan@webmynesystems.com <darpan@webmynesystems.com>
2022-08-10 12:04:55 +0530

    check build

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
           <div class="col-12 col-lg-8 offset-lg-4 home-details text-left text-sm-center text-lg-left">
             <div>
               <img src="../../asset/img/img-mobile.jpg" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="my picture" />
-              <h1 class="text-uppercase poppins-font">I'm steve milner.<span>designer</span></h1>
+              <h1 class="text-uppercase poppins-font">I'm steve milner.<span>web designer</span></h1>
               <p class="open-sans-font">I'm a Tunisian based web designer & front‑end developer focused on crafting clean & user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
               <a class="button" href="about.html">
                 <span class="button-text">more about me</span>
